e52a13e26681ca4996bf1b216a58f17c9762d242
Ravi <raviy3083@gmail.com>
2026-04-04 02:57:18 +0530

    drizzle ssl added

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,18 @@
 import { defineConfig } from "drizzle-kit";
 import { ENV } from "./src/config/env";
+import type { Config } from "drizzle-kit";
+
+export default {
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql", // ✅ REQUIRED
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+} satisfies Config;
 
 // export default defineConfig({
 //   schema: "./src/db/schema.ts",
@@ -13,14 +26,14 @@ import { ENV } from "./src/config/env";
   
 // });
 
-export default {
-  schema: "./src/db/schema.ts",
-  out: "./drizzle",
-  driver: "pg",
-  dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
-};
+// export default {
+//   schema: "./src/db/schema.ts",
+//   out: "./drizzle",
+//   driver: "pg",
+//   dbCredentials: {
+//     connectionString: process.env.DATABASE_URL!,
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   },
+// };
